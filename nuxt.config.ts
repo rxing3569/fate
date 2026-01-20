0e9189910c48929f27302a322f08f05e429caa3a
Berglas <sasuke01260616@gmail.com>
2026-01-20 21:25:39 +0800

    chore: reset baseURL to root for custom domain support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
     '@vueuse/nuxt'
   ],
   app: {
-    baseURL: '/fate/',
+    baseURL: '/',
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       link: [
